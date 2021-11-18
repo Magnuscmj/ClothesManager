57c02286c75eb1aa4cf518add7a5af8b05d73a79
Petter Haugland <petterhaugland@gmail.com>
2021-11-18 22:39:58 +0100

    Temp PUT

--- src/components/products/Products.tsx
+++ src/components/products/Products.tsx
@@ -25,13 +25,20 @@ function Products() {
     getProducts()
   }
 
+<<<<<<< Updated upstream
   const updateProduct = async (id: string, data: IProduct) => {
     await productService.updateProduct(id, data);
   }
+=======
+  const updateProduct = async (data: IProduct, id?: string) => {
+    await productService.updateProducts(data, id);
+  };
+>>>>>>> Stashed changes
 
   // bootstrap card-display
   const createProductList = () => {
     return products.map((product: IProduct, key: number) => {
+<<<<<<< Updated upstream
       console.log(product.id);
       
       return (
@@ -50,6 +57,43 @@ function Products() {
             </Card.Body>
           </Card>
         </div>
+=======
+      const dummyObject: IProduct = {
+        id: product.id,
+        name: 'putasdasfsasfd',
+        type: 'put',
+        image: ''
+      }
+      return (
+        <Col key={key}>
+          <div className='card-container'>
+            <Card className='Cards' style={{ width: '18rem', }}>
+              <Card.Img
+                style={{ maxWidth: '350px', minHeight: '200px' }}
+                variant='top'
+                src={`https://localhost:5001/images/${product.image}`}
+              />
+              <Card.Body>
+                <Card.Title> {product.name} </Card.Title>
+                <Card.Text> {product.type} </Card.Text>
+                <Button
+                  variant='primary'
+                  style={{ marginRight: '10px', minWidth: '100px' }}
+                  onClick={() => updateProduct(dummyObject, product.id)}
+                >
+                  Edit
+                </Button>
+                <Button
+                  variant='danger'
+                  onClick={() => deleteProductByID(product.id)}
+                  style={{ marginLeft: '10px', minWidth: '100px' }}
+                >
+                  Delete
+                </Button>
+              </Card.Body>
+            </Card>
+          </div>
+>>>>>>> Stashed changes
         </Col>
       );
     });
--- src/services/productService.ts
+++ src/services/productService.ts
@@ -33,10 +33,18 @@ export const productService = (function () {
     await axios.delete(`https://localhost:5001/products/${id}`)
   }
 
+<<<<<<< Updated upstream
   //Update
   const updateProduct = async (id: string, data: IProduct) => {
     await axios.put(`https://localhost:5001/products/${id}`, data)
   }
+=======
+  //PUT
+  const updateProducts = async (data: IProduct, id?: string) => {
+    console.log(id);
+    await axios.put(`https://localhost:5001/products/${id}`, data);
+  };
+>>>>>>> Stashed changes
 
   // returns the requests
   return {
